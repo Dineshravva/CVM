ea0d6bc169e54046970cb5de8e0ab5e1947a8941
rehan-005 <rehan63092@gmail.com>
2026-05-27 12:19:38 +0530

    Add Phase 1 REPL support

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,174 @@
 #include "Compiler.h"
 #include "VM.h"
 #include "Disassembler.h"
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: cvm <script.cvm>\n";
-        return 1;
-    }
+int main(int argc,char* argv[])
+{
+    Compiler compiler;
+    VM vm;
 
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << argv[1] << "\n";
-        return 1;
-    }
+    try{
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string source = buffer.str();
+        // REPL mode
 
-    try {
-        Lexer lexer(source);
-        std::vector<Token> tokens = lexer.tokenize();
-        
-        Parser parser(tokens);
-        auto stmts = parser.parse();
+        if(argc==1){
 
-        Compiler compiler;
-        std::vector<uint8_t> bytecode = compiler.compile(stmts);
+            std::cout
+            <<"===== CVM REPL =====\n";
 
-        // Print bytecode
-        // std::cout << "--- Bytecode ---" << std::endl;
-        // for (size_t i = 0; i < bytecode.size(); i++) {
-        //     std::cout << std::hex << (int)bytecode[i] << " ";
-        // }
-        // std::cout << std::dec << "\n----------------\n" << std::endl;
-        
+            std::cout
+            <<"Type exit to quit\n\n";
 
-        //new bytecode better
-        Disassembler::print(
-            bytecode,
-            compiler.getVariableNames()
+            while(true){
+
+                std::cout
+                <<"CVM> ";
+
+                std::string source;
+
+                std::getline(
+                std::cin,
+                source
+                );
+
+                if(source=="exit"){
+
+                    break;
+                }
+
+                if(source.empty()){
+
+                    continue;
+                }
+
+                try{
+
+                    Lexer lexer(source);
+
+                    auto tokens=
+                    lexer.tokenize();
+
+                    Parser parser(tokens);
+
+                    auto stmts=
+                    parser.parse();
+
+                    auto bytecode=
+
+                    compiler.compile(
+                    stmts
+                    );
+
+                    Disassembler::print(
+
+                    bytecode,
+
+                    compiler.getVariableNames()
+
+                    );
+
+                    vm.execute(
+                    bytecode
+                    );
+
+                }
+
+                catch(
+                const std::exception& e
+                ){
+
+                    std::cerr
+
+                    <<"Error: "
+
+                    <<e.what()
+
+                    <<"\n";
+                }
+            }
+
+            return 0;
+        }
+
+        // Old script mode
+
+        if(argc!=2){
+
+            std::cerr
+            <<"Usage: cvm <script.cvm>\n";
+
+            return 1;
+        }
+
+        std::ifstream file(
+        argv[1]
         );
 
-        VM vm;
-        vm.execute(bytecode);
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << "\n";
+        if(!file.is_open()){
+
+            std::cerr
+
+            <<"Failed to open file: "
+
+            <<argv[1]
+
+            <<"\n";
+
+            return 1;
+        }
+
+        std::stringstream buffer;
+
+        buffer<<file.rdbuf();
+
+        std::string source=
+        buffer.str();
+
+        Lexer lexer(source);
+
+        auto tokens=
+        lexer.tokenize();
+
+        Parser parser(tokens);
+
+        auto stmts=
+        parser.parse();
+
+        auto bytecode=
+        compiler.compile(
+        stmts
+        );
+
+        Disassembler::print(
+
+        bytecode,
+
+        compiler.getVariableNames()
+
+        );
+
+        vm.execute(
+        bytecode
+        );
+
+    }
+
+    catch(
+    const std::exception& e
+    ){
+
+        std::cerr
+
+        <<"Error: "
+
+        <<e.what()
+
+        <<"\n";
+
         return 1;
     }
 
